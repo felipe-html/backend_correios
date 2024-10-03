c0cfca4553d9219d5d76eec22f0e7d0bc4a13135
tiagohumberto <rezende.tiago@gmail.com>
2024-10-03 17:18:39 -0300

    criação rota patrimonios

---
+++ src/app/patrimonio/entradas/outras-entrada/outras-entrada.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-outras-entrada',
+  standalone: true,
+  imports: [],
+  templateUrl: './outras-entrada.component.html',
+  styleUrl: './outras-entrada.component.css'
+})
+export class OutrasEntradaComponent {
+
+}
